e8bf43715068d70c489f47fadff926537b9c37de
Ivan <ivanshatukho@yandex.ru>
2018-12-24 18:59:24 +0300

    Indents

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -14,8 +14,14 @@ def not_found(error):
 
 @app.route('/api/v1.0/tasks/getAll', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': TaskSchema(many=True).dump(
-        Interaction_DB.get_all(named=request.args.get('title', default=False))).data or 'Title not found'})
+    return jsonify(
+        {
+            'tasks': TaskSchema(many=True).dump(
+                Interaction_DB.get_all(
+                    named=request.args.get('title', default=False)
+                )
+            ).data or 'Title not found'}
+    )
 
 
 @app.route('/api/v1.0/tasks/get', methods=['GET'])
@@ -29,19 +35,35 @@ def create_task():
     try:
         data, errors = TaskSchema().load(request.get_json())
         Interaction_DB.post(**data)
-        return jsonify({'task': TaskSchema().dump(Interaction_DB.get_one_by_title(data['title'])).data}), 201
+        return jsonify(
+            {
+                'task': TaskSchema().dump(
+                    Interaction_DB.get_one_by_title(data['title'])
+                ).data
+            }
+        ), 201
     except Exception as e:
         return jsonify({'errors': e.messages}), 400
 
 
 @app.route('/api/v1.0/tasks/update', methods=['PUT'])
 def update_task():
-    response = Interaction_DB.update_one_by_title(request.args.get('title'), request.json.get('title', request.args.get('title')),
-                                       request.json['description'], request.json.get('done', False))
+    response = Interaction_DB.update_one_by_title(
+        request.args.get('title'),
+        request.json.get('title', request.args.get('title')),
+        request.json['description'],
+        request.json.get('done', False)
+    )
     return jsonify({'task': TaskSchema().dump(response).data}), 200
 
 
 @app.route('/api/v1.0/tasks/delete', methods=['DELETE'])
 def delete_task():
-    return jsonify({'result': 'Success', 'deleted task': TaskSchema().dump(
-        Interaction_DB.delete_one_by_title(request.args.get('title'))).data}), 200
+    return jsonify(
+        {
+            'result': 'Success',
+            'deleted task': TaskSchema().dump(
+                Interaction_DB.delete_one_by_title(request.args.get('title'))
+            ).data
+        }
+    ), 200
